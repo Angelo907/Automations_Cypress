7940a059ab07f5b851ceea82b5a617270442ed0b
angelo <[angelofranccesco@gmail.com>
2026-06-12 12:47:47 -0500

    primer-commit-cypress

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,7 @@
+//describe es como si fuera el feature en selenium, es la funcionalidad, ejem: login
+//it sirve para declara el test y passes es el nombre del test
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
